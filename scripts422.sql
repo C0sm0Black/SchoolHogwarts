3cfad350db60d755a481527db9b9428c20419e89
CosmoBlack <soulker_mozyr@yahoo.com>
2023-05-02 22:16:41 +0200

    Homework 4-2

---
+++ scripts422.sql
@@ -0,0 +1,20 @@
+CREATE TABLE cars
+(
+
+    id    INTEGER PRIMARY KEY,
+    brand TETX,
+    model TEXT,
+    price NUMERIC(10, 2)
+
+);
+
+CREATE TABLE persons
+(
+
+    id       INTEGER PRIMARY KEY,
+    name     TEXT,
+    age      INTEGER,
+    canDrive BOOLEAN,
+    car_id   TETX REFERENCES cars (id)
+
+);
