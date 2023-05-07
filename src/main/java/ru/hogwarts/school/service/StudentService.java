667c3741c51f5fa1a339193bbe7a3862e45e59f2
CosmoBlack <soulker_mozyr@yahoo.com>
2023-05-07 12:35:48 +0200

    Homework 4-4

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
@@ -10,6 +12,8 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final Logger logger = LoggerFactory.getLogger(StudentService.class);
+
     private final StudentRepository studentRepository;
 
     public StudentService(StudentRepository studentRepository) {
@@ -17,34 +21,57 @@ public class StudentService {
     }
 
     public Student createStudent(Student student) {
+
+        logger.info("Was invoked method StudentService::createStudent");
         return studentRepository.save(student);
+
     }
 
     public Student findStudent(long id) {
+
+        logger.info("Was invoked method StudentService::findStudent");
         return studentRepository.getById(id);
+
     }
 
     public Student editStudent(Student student) {
+
+        logger.info("Was invoked method StudentService::editStudent");
         return studentRepository.save(student);
+
     }
 
     public void deleteStudent(long id) {
+
+        logger.info("Was invoked method StudentService::deleteStudent");
         studentRepository.deleteById(id);
+
     }
 
     public List<Student> getStudentsByAge(int age) {
+
+        logger.info("Was invoked method StudentService::getStudentsByAge");
         return studentRepository.findAllByAge(age);
+
     }
 
     public List<Student> getAll() {
+
+        logger.info("Was invoked method StudentService::getAll");
         return studentRepository.findAll();
+
     }
 
     public List<Student> getStudentByAgeBetween(int min, int max) {
+
+        logger.info("Was invoked method StudentService::getStudentByAgeBetween");
         return studentRepository.findAllByAgeBetween(min, max);
+
     }
 
     public Faculty getFacultyByStudent(String name) {
+
+        logger.info("Was invoked method StudentService::getFacultyByStudent");
 
         Student student = studentRepository.findFirstByName(name);
         return student.getFaculty();
@@ -52,15 +79,24 @@ public class StudentService {
     }
 
     public Long getAmountStudents() {
+
+        logger.info("Was invoked method StudentService::getAmountStudents");
         return studentRepository.getAmountStudents();
+
     }
 
     public Double getAvgAgeStudents() {
+
+        logger.info("Was invoked method StudentService::getAvgAgeStudents");
         return studentRepository.getAvgAgeStudents();
+
     }
 
     public List<Student> getLastStudents() {
+
+        logger.info("Was invoked method StudentService::getLastStudents");
         return studentRepository.getLastStudents();
+
     }
 
 }
